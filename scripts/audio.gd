0259a207f65d09b3b0dcfacf60fa23ee9a86031a
Lenchog <lennyescott@gmail.com>
2024-11-24 09:40:07 +1100

    added placeholder monster and basic spacial audio
    added placeholder monster and basic spacial audio
    
    added placeholder monster and basic spacial audio

---
+++ scripts/audio.gd
@@ -0,0 +1,25 @@
+extends AudioStreamPlayer3D
+
+var effect
+var recording
+
+
+func _ready():
+	# We get the index of the "Record" bus.
+	var idx = AudioServer.get_bus_index("Record")
+	# And use it to retrieve its first effect, which has been defined
+	# as an "AudioEffectRecord" resource.
+	effect = AudioServer.get_bus_effect(idx, 0)
+	if effect.is_recording_active():
+		recording = effect.get_recording()
+		$PlayButton.disabled = false
+		$SaveButton.disabled = false
+		effect.set_recording_active(false)
+		$RecordButton.text = "Record"
+		$Status.text = ""
+	else:
+		$PlayButton.disabled = true
+		$SaveButton.disabled = true
+		effect.set_recording_active(true)
+		$RecordButton.text = "Stop"
+		$Status.text = "Recording..."
